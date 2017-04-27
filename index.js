758a0269b7b7f43481c9a7b030b5e08960cddb38
vporta <vporta7@gmail.com>
2017-04-27 17:27:05 -0400

    market data

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const Promise = require('bluebird');
 const EventEmitter = require('events');
-
-const HOST = 'https://api.tradier.com/v1/';
+const axios = require('axios');
+const HOST = 'https://sandbox.tradier.com/v1/';
     
-class Stocks {
+class Tradier {
   constructor(accesstoken) {
     this.accesstoken = accesstoken;
 
@@ -12,15 +12,15 @@ class Stocks {
     }
   }
 
-  quote(ticker, callback) {
+  quote(ticker='', callback) {
     return axios.get(`${HOST}markets/quotes?symbols=${ticker}`, {
       headers: {
-        "Authorization": `Bearer  ${this.accesstoken}`
+        "Authorization": `Bearer ${this.accesstoken}`
       }
     })
       .then(response => {
-        const { data } = response.data;
-        callback(data);
+        const { quotes } = response.data;
+        callback(quotes);
       })
       .catch(error => {
         console.log(error);
@@ -30,27 +30,27 @@ class Stocks {
   timesales(ticker, callback) { 
     return axios.get(`${HOST}markets/timesales?symbol=${ticker}`, { 
       headers: {
-        "Authorization": `Bearer  ${this.accesstoken}` 
+        "Authorization": `Bearer ${this.accesstoken}` 
       } 
     })
       .then(response => {
-        const { data } = response.data;
-        callback(data);
+        const { series } = response.data;
+        callback(series);
       })
       .catch(error => {
         console.log(error);
       });
   }
 
-  optionchain(ticker, callback) { 
-    return axios.get(`${HOST}markets/options/chains?symbol=${ticker}`, { 
+  optionchain(ticker, expiration, callback) { 
+    return axios.get(`${HOST}markets/options/chains?symbol=${ticker}&expiration=${expiration} `, { 
       headers: {
-        "Authorization": `Bearer  ${this.accesstoken}` 
+        "Authorization": `Bearer ${this.accesstoken}` 
       } 
     })
       .then(response => {
-        const { data } = response.data;
-        callback(data);
+        const { options } = response.data;
+        callback(options);
       })
       .catch(error => {
         console.log(error);
@@ -60,12 +60,12 @@ class Stocks {
   optionstrikes(ticker, expiration, callback) { 
     return axios.get(`${HOST}markets/options/strikes?symbol=${ticker}&expiration=${expiration}`, { 
       headers: {
-        "Authorization": `Bearer  ${this.accesstoken}` 
+        "Authorization": `Bearer ${this.accesstoken}` 
       } 
     })
       .then(response => {
-        const { data } = response.data;
-        callback(data);
+        const { strikes } = response.data;
+        callback(strikes);
       })
       .catch(error => {
         console.log(error);
@@ -75,12 +75,12 @@ class Stocks {
   optionexpirations(ticker, callback) { 
     return axios.get(`${HOST}markets/options/expirations?symbol=${ticker}`, { 
       headers: {
-        "Authorization": `Bearer  ${this.accesstoken}` 
+        "Authorization": `Bearer ${this.accesstoken}` 
       } 
     })
       .then(response => {
-        const { data } = response.data;
-        callback(data);
+        const { expirations } = response.data;
+        callback(expirations);
       })
       .catch(error => {
         console.log(error);
@@ -90,11 +90,26 @@ class Stocks {
   historical(ticker, callback) { 
     return axios.get(`${HOST}markets/history?symbol=${ticker}`, { 
       headers: {
-        "Authorization": `Bearer  ${this.accesstoken}` 
+        "Authorization": `Bearer ${this.accesstoken}` 
       } 
     })
       .then(response => {
-        const { data } = response.data;
+        const { history } = response.data;
+        callback(history);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
+  intradaystatus(callback) {
+    return axios.get(`${HOST}markets/clock`, { 
+      headers: {
+        "Authorization": `Bearer ${this.accesstoken}` 
+      } 
+    })
+      .then(response => {
+        const { data } = response;
         callback(data);
       })
       .catch(error => {
@@ -102,29 +117,14 @@ class Stocks {
       });
   }
 
-  intradaystatus() {
-    return axios.get(`markets/clock`, { 
+  marketcalendar(callback) {
+    return axios.get(`${HOST}markets/calendar`, { 
       headers: {
-        "Authorization": `Bearer  ${this.accesstoken}` 
+        "Authorization": `Bearer ${this.accesstoken}` 
       } 
     })
       .then(response => {
-        const { data } = response.data;
-        callback(data);
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  }
-
-  marketcalendar() {
-    return axios.get(`markets/calendar`, { 
-      headers: {
-        "Authorization": `Bearer  ${this.accesstoken}` 
-      } 
-    })
-      .then(response => {
-        const { data } = response.data;
+        const { data } = response;
         callback(data);
       })
       .catch(error => {
@@ -133,9 +133,9 @@ class Stocks {
   }
 
   companysearch(ticker) {
-    return axios.get(`markets/search?q=${ticker}`, { 
+    return axios.get(`${HOST}markets/search?q=${ticker}`, { 
       headers: {
-        "Authorization": `Bearer  ${this.accesstoken}` 
+        "Authorization": `Bearer ${this.accesstoken}` 
       } 
     })
       .then(response => {
@@ -156,3 +156,11 @@ class Stocks {
   }
 
 }
+
+const tradier = new Tradier('flbnuhYahicB86qobTxWSTgLN3aL');
+
+tradier.intradaystatus(data => {
+  console.log(data);
+});
+
+// export default Tradier;
